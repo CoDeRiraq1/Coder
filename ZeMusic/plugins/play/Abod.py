f80cd8adfc4b5e3b67740872d4118477bae64710
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2024-05-05 21:49:48 +0300

    Update Abod.py

--- ZeMusic/plugins/play/Abod.py
+++ ZeMusic/plugins/play/Abod.py
@@ -177,7 +177,7 @@ async def ihd(client: Client, message: Message):
 async def ihd(client: Client, message: Message):
     rl = random.randint(2,90)
     url = f"https://t.me/M_Z_J1/{rl}"
-    await client.send_mp3(message.chat.id,url,caption="↯ by - @M_Z_J1 : تم اختيار المزج لك 🤍",
+    await client.send(message.chat.id,url,caption="↯ by -  : تم اختيار المزج لك 🤍",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
